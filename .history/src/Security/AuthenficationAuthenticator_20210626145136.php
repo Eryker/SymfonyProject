1c32fd872b749fff3cc222c2d73a47398ddb4548
Oumar <oumar.youssouf0605@gmail.com>
2021-06-26 18:37:32 +0200

    Adding navbar

--- .history/src/Security/AuthenficationAuthenticator_20210626145136.php
+++ .history/src/Security/AuthenficationAuthenticator_20210626145136.php
@@ -95,7 +95,8 @@ class AuthenficationAuthenticator extends AbstractFormLoginAuthenticator impleme
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('app_home'));
+        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
